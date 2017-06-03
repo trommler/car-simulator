55a9680260e202dab515fc2cb5ac47873f974c67
Florian Bauer <florian.bauer.dev@gmail.com>
2017-06-03 15:38:32 +0200

    restricted the class EcuLuaScript to move-only and removed member raw pointer to avoid potential dangling references

--- src/ecu_lua_script.h
+++ src/ecu_lua_script.h
@@ -10,6 +10,7 @@
 #include "lua.hpp"
 #include <string>
 #include <cstdint>
+#include <memory>
 
 constexpr char REQ_ID_FIELD[] = "RequestId";
 constexpr char RES_ID_FIELD[] = "ResponseId";
@@ -20,19 +21,19 @@ class EcuLuaScript
     
 public:
     EcuLuaScript() = delete;
-    EcuLuaScript(const EcuLuaScript& origin) = default;
-    EcuLuaScript& operator =(const EcuLuaScript& origin) = default;
+    EcuLuaScript(const EcuLuaScript& origin) = delete;
+    EcuLuaScript& operator =(const EcuLuaScript& origin) = delete;
     EcuLuaScript(EcuLuaScript&& origin) = default;
     EcuLuaScript& operator =(EcuLuaScript&& origin) = default;
     EcuLuaScript(const std::string& luaScript);
-    virtual ~EcuLuaScript();
+    virtual ~EcuLuaScript() = default;
 
     std::uint16_t getRequestId() const;
     std::uint16_t getResponseId() const;
     const std::string getDataByIdentifier(std::uint16_t identifier) const;
 
 private:
-    sel::State* lua_state_ = nullptr;
+    sel::State lua_state_;
     std::uint16_t request_id_ = 0;
     std::uint16_t response_id_ = 0;
 
--- src/main.cpp
+++ src/main.cpp
@@ -28,69 +28,6 @@
 
 using namespace std;
 
-void luaTest()
-{
-    const string test_script = "tests/testscript01.lua";
-
-    if (utils::existsFile(test_script))
-    {
-        sel::State state;
-        state.Load(test_script);
-
-        cout << "The Answer: " << int(state["theAnswer"]) << '\n';
-        const string cmd = state["bashCommand"];
-        cout << "The Bash command everyone should try out: \"" << cmd << "\"\n";
-    }
-}
-
-void isotpCanTest()
-{
-    struct sockaddr_can addr;
-
-    addr.can_addr.tp.tx_id = 0x123; // sender
-    addr.can_addr.tp.rx_id = 0x321; // receiver
-
-    int skt = socket(PF_CAN, SOCK_DGRAM, CAN_ISOTP);
-    if (skt < 0)
-    {
-        cerr << __func__ << "() socket: " << strerror(errno) << '\n';
-        exit(1);
-    }
-
-    addr.can_family = AF_CAN;
-
-    struct ifreq ifr;
-    constexpr char dev[] = "vcan0";
-    strncpy(ifr.ifr_name, dev, sizeof(dev));
-    ioctl(skt, SIOCGIFINDEX, &ifr);
-    addr.can_ifindex = ifr.ifr_ifindex;
-
-    auto bind_res = bind(skt,
-                         reinterpret_cast<struct sockaddr*>(&addr),
-                         sizeof(addr));
-    if (bind_res < 0)
-    {
-        cerr << __func__ << "() bind: " << strerror(errno) << '\n';
-        close(skt);
-        exit(1);
-    }
-
-    constexpr array<uint8_t, 16> buf = {
-        0x10, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
-    };
-
-    auto bytes_sent = write(skt, buf.data(), buf.size());
-    if (bytes_sent < 0)
-    {
-        cerr << __func__ << "() write: " << strerror(errno) << '\n';
-        return;
-    }
-
-    cerr << __func__ << "() sent " << bytes_sent << " bytes\n";
-    close(skt);
-}
-
 /**
  * The main application only for testing purposes.
  *
@@ -100,8 +37,6 @@ void isotpCanTest()
  */
 int main(int argc, char** argv)
 {
-    luaTest(); // test function to get some values from a Lua script
-
     constexpr array<uint8_t, 12> payload01 = {
         0x10, 0x59, 0x65, 0x73, 0x20, 0x77, 0x65, 0x20, 0x43, 0x41, 0x4E, 0x21
     };
